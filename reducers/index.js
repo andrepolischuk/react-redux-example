c5cfcc56163409518002be898e7385e01ddda551
andrepolischuk <andre.polischuk@gmail.com>
2016-03-23 14:54:35 +0300

    Extract reducer to separate module

--- reducers/index.js
+++ reducers/index.js
@@ -1,26 +1,5 @@
 import { combineReducers } from 'redux';
-import { REQUEST_USER, RECEIVE_USER } from '../constants/ActionTypes';
-
-const initialState = {
-  isFetching: false,
-  data: {}
-};
-
-function user(state = initialState, action) {
-  switch (action.type) {
-    case REQUEST_USER:
-      return Object.assign({}, state, {
-        isFetching: true
-      });
-    case RECEIVE_USER:
-      return Object.assign({}, state, {
-        isFetching: false,
-        data: action.data
-      });
-    default:
-      return state;
-  }
-}
+import user from './user';
 
 const rootReducer = combineReducers({
   user
