cb10b78863d5f958737242ef5189b4388f5f9896
andrepolischuk <andre.polischuk@gmail.com>
2016-03-29 14:58:21 +0300

    Add webpack dedupe plugin

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -17,6 +17,7 @@ const config = {
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(env)
     })
